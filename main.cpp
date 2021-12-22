9dd7dde5bcb30d17cd9893850a1a60757af1611c
drekavak <jang9434@gmail.com>
2021-12-22 13:52:30 +0900

    비쥬얼 스튜디오 삼각형 넓이 구하기

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float base = 0.0f;
+	float height = 0.0f;
+	float area = 0.0f;
+
+	cout << "삼각형의 밑변의 길이는?" << endl;
+	cin >> base;
+	cout << "삼각형의 높이의 길이는?" << endl;
+	cin >> height;
+	area = base * height / 2;
+	cout << "삼각형의 넓이는 " << area << " 입니다." << endl;
+
+	return 0;
+
+}
